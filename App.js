ee3ea6d734490fb669aa6465d9eb19eac580c11c
Pujitha Mallampalli <poojithampalli@gmail.com>
2023-08-23 01:18:59 -0700

    app working

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heaidng">Namaste React Functional Component</h1>
+    </div>
+    <div id="container-2"></div>
+  </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
